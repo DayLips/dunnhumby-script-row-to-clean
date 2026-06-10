fa4b1790b95aa3e88a7487ddb3726211a633165b
DayLips <daylips312@gmail.com>
2026-06-10 22:50:18 +0300

    init database.py, main.py, models

---
+++ main.py
@@ -0,0 +1,15 @@
+import logging
+
+from database import init_db, SessionLocal
+
+def main():
+    init_db()
+    
+    db = SessionLocal()
+    try:
+        ...
+    finally:
+        db.close()
+
+if __name__ == '__main__':
+    main()
